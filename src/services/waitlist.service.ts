5b0af3e7bb7882caf83ec29b9868c6dba6272730
janvi-lalwani <janvi.lalwani@bosctechlabs.com>
2025-12-26 14:30:44 +0530

    refactor: update waitlist service to use WaitlistUser entity directly

--- src/services/waitlist.service.ts
+++ src/services/waitlist.service.ts
@@ -1,5 +1,5 @@
 import { getDataSource } from "../lib/data-source";
-import { WaitlistUser, WaitlistUserSchema } from "../entities/WaitlistUser";
+import { WaitlistUser } from "../entities/WaitlistUser";
 
 export class WaitlistService {
   static async upsertUser(data: {
@@ -11,7 +11,7 @@ export class WaitlistService {
     console.log("Service: Upserting user", data.email);
     const dataSource = await getDataSource();
     console.log("Service: DataSource initialized");
-    const repository = dataSource.getRepository(WaitlistUserSchema);
+    const repository = dataSource.getRepository(WaitlistUser);
 
     let user = await repository.findOneBy({ email: data.email });
     console.log("Service: Existing user found:", !!user);
